bef27c5abd02e772d641ad4b56c8432e189ac6f2
Vik <jzenterprize90@gmail.com>
2021-05-17 23:48:29 -0400

    Changed comments

--- TheSetDefault.py
+++ TheSetDefault.py
@@ -3,9 +3,11 @@ box = {'name': 'Pooka', 'age': 5}
 # if 'color' not in box:
 #     box['color'] = 'black'
 
-# setdefault is a way to do this with code
-# First argument passed to the method is the key to check for, and the second argument is the value to set at that key
-# if the key doesn't exist
+'''
+setdefault is a way to do this with code
+First argument passed to the method is the key to check for, and the second argument is the value to set at that key
+if the key doesn't exist
+'''
 
 box.setdefault('color', 'black')
 
